a1e73aad8a565152da568d3c428af3142204f207
Mark Nielsen <nielsen.markus11@gmail.com>
2017-06-03 01:27:23 -0600

    Add initial files.

---
+++ create-htmlwidget_files/hive-binding-0.0.0.9000/hive.js
@@ -0,0 +1,121 @@
+HTMLWidgets.widget({
+
+  name: 'hive',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        // alias options
+        var options = x.options;
+
+        // convert links and nodes data frames to d3 friendly format
+        var nodes = HTMLWidgets.dataframeToD3(x.nodes);
+        var prelinks = HTMLWidgets.dataframeToD3(x.links);
+
+        // create json of link sources and targets
+        var links = [];
+        prelinks.forEach(function(d){
+          var tmp = {};
+          tmp.source=nodes[d.source];
+          tmp.target=nodes[d.target];
+          links.push(tmp);
+        });
+
+
+        var innerRadius = options.innerRadius,
+            outerRadius = options.outerRadius;
+
+        var angle = d3.scale.ordinal().domain(d3.range(x.numAxis+1)).rangePoints([0, 2 * Math.PI]),
+            radius = d3.scale.linear().range([innerRadius, outerRadius]),
+            color = d3.scale.category10().domain(d3.range(20));
+
+        // select the svg element and remove existing children
+        var svg = d3.select(el).append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        svg.selectAll(".axis")
+            .data(d3.range(x.numAxis))
+            .enter().append("line")
+            .attr("class", "axis")
+            .attr("transform", function(d) {
+              return "rotate(" + degrees(angle(d)) + ")";
+            })
+            .attr("x1", radius.range()[0])
+            .attr("x2", radius.range()[1]);
+
+        // draw links
+        var link = svg.selectAll(".link")
+            .data(links)
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", d3.hive.link()
+              .angle(function(d) { return angle(d.x); })
+              .radius(function(d) { return radius(d.y); }))
+            .style("stroke", function(d) { return color(d.source.color); })
+            .style("stroke-width", 1.5)
+            .style("opacity", options.opacity)
+            .on("mouseover", function(d) {
+              d3.select(this)
+                .style("opacity", 1)
+                .style("stroke", "#ff0000")
+                .style("stroke-width", 2);
+            })
+            .on("mouseout", function(d) {
+              d3.select(this)
+                .style("opacity", options.opacity)
+                .style("stroke", function(d) { return color(d.source.color); })
+                .style("stroke-width", 1.5);
+            });
+
+        // draw nodes
+        var node = svg.selectAll(".node")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+              return "rotate(" + degrees(angle(d.x)) + ")";
+            })
+            .attr("cx", function(d) { return radius(d.y); })
+            .attr("r", 5)
+            .style("fill", function(d) { return color(d.color); })
+            .style("stroke", "#000000")
+            .on("mouseover", function(d) {
+              d3.select(this)
+                .style("stroke", "#ff0000");
+
+              link.style("stroke", function(l) {
+                  return d === l.source || d === l.target ? "#ff0000" : color(l.source.color);
+                })
+                .style("stroke-width", function(l) {
+                  return d === l.source || d === l.target ? 2 : 1.5;
+                });
+            })
+            .on("mouseout", function(d) {
+              d3.select(this)
+                .style("stroke", "#000000");
+
+              link.style("stroke", function(l) {
+                return color(l.source.color);
+                })
+                .style("stroke-width", 1.5);
+            });
+
+        function degrees(radians) {
+          return radians / Math.PI * 180 - 90;
+        }
+
+      }
+
+    };
+  }
+});
